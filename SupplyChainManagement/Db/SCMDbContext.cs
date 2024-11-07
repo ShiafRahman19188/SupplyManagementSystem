5c37569655727211897a85ab3e4b9a30eb3bff07
Md. Noor Alam <noor.alam@epylliongroup.com>
2024-11-07 09:23:21 +0600

    comit

--- SupplyChainManagement/Db/SCMDbContext.cs
+++ SupplyChainManagement/Db/SCMDbContext.cs
@@ -15,18 +15,6 @@ namespace SupplyChainManagement.Db
         public DbSet<Merchandiser> Merchandisers { get; set; }
         public DbSet<DeliveryUnit> DeliveryUnits { get; set; }
         public DbSet<PRDetails> ItemDetails { get; set; }
-<<<<<<< HEAD
-        public DbSet<YarnPOMaster> ItemPOMaster { get; set; }
-        public DbSet<YarnPOChild> ItemPODetail { get; set; }
-=======
-        public DbSet<Yarn> Yarns { get; set; }
-        public DbSet<ItemMaster> ItemMasters { get; set; }
-        public DbSet<ItemGroup> ItemGroups { get; set; }
-        public DbSet<ItemSubGroup> ItemSubGroups { get; set; }
-        public DbSet<BookingMaster> BookingMasters { get; set; }
-        public DbSet<BookingChild> BookingChild { get; set; }
-        public DbSet<FabricYarn> FabricYarns { get; set; }
->>>>>>> a974eb185cbe63fb120b8be69b0f820ec8a95a9c
 
 
 

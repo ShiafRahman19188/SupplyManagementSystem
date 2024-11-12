9ab3c3c7e8574835e9c84816ceb433d076e47461
Md. Noor Alam <noor.alam@epylliongroup.com>
2024-11-12 16:33:58 +0600

    commit

--- SupplyChainManagement/DTO/BookingInfoDto.cs
+++ SupplyChainManagement/DTO/BookingInfoDto.cs
@@ -140,6 +140,18 @@ namespace SupplyChainManagement.DTO
     {
         public YarnPOMaster? YarnPOMaster { get; set; }
         public List<YarnPOChild>? ItemDetails { get; set; }
+
+
+        public string? PONo { get; set; }
+        public DateTime? PODate { get; set; }
+        public string? SupplierName { get; set; }
+        public decimal? Charges { get; set; }
+        public string? CountryOfOrigin { get; set; }
+        public int? ShippingTolerance { get; set; }
+        public string? PortofLoading { get; set; }
+        public string? PortofDischarge { get; set; }
+        public string? ShipmentMode { get; set; }
+        public List<YarnPOChild>? ItemDetailss { get; set; }
     }
 
     public class ProcurementLandingDto
